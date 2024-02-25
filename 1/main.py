9309b06e875459626885939b9ab2bd0718e0b677
ptukovar <53978671+ptukovar@users.noreply.github.com>
2024-02-25 17:43:14 +0000

    update

--- 1/main.py
+++ 1/main.py
@@ -1,7 +1,33 @@
+import re
 def whitespace(string):
     return string.replace(" ", "")
+
+def checkpar(string):
+    temp = re.sub(r'[^\(\){}\[\]]', '', string)
+    stack = []
+    for char in temp:
+        if char in ["(", "{", "["]:
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            current_char = stack.pop()
+            if current_char == '(':
+                if char != ")":
+                    return False
+            if current_char == '{':
+                if char != "}":
+                    return False
+            if current_char == '[':
+                if char != "]":
+                    return False
+    if stack:
+        return False
+    return True
+        
 
 count = input("Zadejte pocet prikladu:")
 for i in range(int(count)):
     string = input("Zadejte priklad:")
-    print(whitespace(string))
+    if not(checkpar(string)):
+        print("ERROR")
