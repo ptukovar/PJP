8c8e2e0b9bdc8b97c981a8886164f9c2206a7fdb
ptukovar <53978671+ptukovar@users.noreply.github.com>
2024-02-26 14:31:06 +0000

    🐵 Update

--- 1/main.py
+++ 1/main.py
@@ -5,6 +5,7 @@ def whitespace(string):
 def checkpar(string):
     temp = re.sub(r'[^\(\){}\[\]]', '', string)
     stack = []
+
     for char in temp:
         if char in ["(", "{", "["]:
             stack.append(char)
@@ -27,14 +28,46 @@ def checkpar(string):
 
 def checkchars(string):
     allowed = {"0","1","2","3","4","5","6","7","8","9","+","-","*","/","(",")","{","}","[","]"}
+
     for char in string:
         if char not in allowed:
             return False
     return True
 
+def copypart(string, start, length):
+    return string[start:start+length]
+
+def calculate(expression):
+    numbers = [int(num) for num in re.findall(r'\d+', expression)]
+    operators = re.findall(r'[\+\-\*/]', expression)
+    result = numbers[0]
+    
+    for i, operator in enumerate(operators):
+        if operator == '+':
+            result += numbers[i + 1]
+        elif operator == '-':
+            result -= numbers[i + 1]
+        elif operator == '*':
+            result *= numbers[i + 1]
+        elif operator == '/':
+            if numbers[i + 1] == 0:
+                raise ValueError("Division by zero!")
+            result /= numbers[i + 1]
+    
+    return result
+
 def evaluate(string):
     result = 0
-    
+
+    while "(" in string:
+        s = string.find("(")
+        inside = copypart(string, s+1, string.find(")")-s-1)
+        temp = calculate(inside)
+        print("inside: ", inside, "temp: ",temp)
+        string =string.replace(string[s:string.find(")")+1], str(temp))
+        print("string: ",string)
+
+    result = calculate(string)
     return result
 
 count = input("Zadejte pocet prikladu:")
