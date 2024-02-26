66f2c1815381e05ca09c0f1ab56924551bd6bb70
ptukovar <53978671+ptukovar@users.noreply.github.com>
2024-02-26 09:32:42 +0000

    🐵 Update

--- 1/main.py
+++ 1/main.py
@@ -25,13 +25,26 @@ def checkpar(string):
         return False
     return True
 
-def eval(string):
+def checkchars(string):
+    allowed = {"0","1","2","3","4","5","6","7","8","9","+","-","*","/","(",")","{","}","[","]"}
+    for char in string:
+        if char not in allowed:
+            return False
+    return True
+
+def evaluate(string):
     result = 0
+    
     return result
 
 count = input("Zadejte pocet prikladu:")
 for i in range(int(count)):
     string = input("Zadejte priklad:")
+    string = whitespace(string)   
     if not(checkpar(string)):
         print("ERROR")
+    elif not(checkchars(string)):
+        print("ERROR")
+    else:
+        print(evaluate(string))
     
